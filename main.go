0b700c40e44b1627ccea05796762ef347fb80c64
Desmond Stular <dstular@yahoo.com>
2025-04-07 21:29:32 -0600

    Added login, logout and protected routes with session and csrf token auth

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 type Login struct {
@@ -15,9 +16,9 @@ var users = map[string]Login{}
 
 func main() {
 	http.HandleFunc("POST /register", register)
-	http.HandleFunc("/login", login)
+	http.HandleFunc("POST /login", login)
 	http.HandleFunc("/logout", logout)
-	http.HandleFunc("/protected", protected)
+	http.HandleFunc("POST /protected", protected)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -41,17 +42,84 @@ func register(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintln(w, "User registered successfully!")
-	fmt.Printf("%v\n", users)
+	fmt.Printf("username: %v | password: %v\n", username, hashedPassword)
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
+	username := r.FormValue("username")
+	password := r.FormValue("password")
 
+	user, ok := users[username]
+	if !ok || !checkPasswordHash(password, user.HashedPassword) {
+		err := http.StatusUnauthorized
+		http.Error(w, "Invalid username or password:", err)
+		return
+	}
+
+	sessionToken := generateToken(32)
+	csrfToken := generateToken(32)
+
+	// Set session cookie
+	http.SetCookie(w, &http.Cookie{
+		Name:     "session_token",
+		Value:    sessionToken,
+		Expires:  time.Now().Add(24 * time.Hour),
+		HttpOnly: true,
+	})
+
+	// Set CSRF token in a cookie
+	http.SetCookie(w, &http.Cookie{
+		Name:     "csrf_token",
+		Value:    csrfToken,
+		Expires:  time.Now().Add(24 * time.Hour),
+		HttpOnly: false,
+	})
+
+	user.SessionToken = sessionToken
+	user.CSRFToken = csrfToken
+	users[username] = user
+
+	fmt.Fprintln(w, "Login successful!")
 }
 
 func logout(w http.ResponseWriter, r *http.Request) {
+	if err := Authorize(r); err != nil {
+		statusError := http.StatusUnauthorized
+		http.Error(w, "Unauthorized", statusError)
+		return
+	}
 
+	// Clear cookies
+	http.SetCookie(w, &http.Cookie{
+		Name:     "session_token",
+		Value:    "",
+		Expires:  time.Now().Add(-time.Hour),
+		HttpOnly: true,
+	})
+	http.SetCookie(w, &http.Cookie{
+		Name:     "csrf_token",
+		Value:    "",
+		Expires:  time.Now().Add(-time.Hour),
+		HttpOnly: false,
+	})
+
+	// Clear the tokens from the database
+	username := r.FormValue("username")
+	user := users[username]
+	user.SessionToken = ""
+	user.CSRFToken = ""
+	users[username] = user
+
+	fmt.Fprintln(w, "Logged out successfully!")
 }
 
 func protected(w http.ResponseWriter, r *http.Request) {
+	if err := Authorize(r); err != nil {
+		statusErr := http.StatusUnauthorized
+		http.Error(w, "Unauthorized", statusErr)
+		return
+	}
 
+	username := r.FormValue("username")
+	fmt.Fprintf(w, "CSRF validation successful! Welcome %s\n", username)
 }
